7d3a3c206eb4c2d3311176af093d0840d345f352
Trần Phú Nguyện <1612448@student.hcmus.edu.vn>
2018-10-13 16:27:04 +0700

    Fix conflict

--- 04_Interface/I02_Source/JobIT/app/src/main/java/com/example/team32gb/jobit/SignInActivity.java
+++ 04_Interface/I02_Source/JobIT/app/src/main/java/com/example/team32gb/jobit/SignInActivity.java
@@ -1,15 +1,10 @@
 package com.example.team32gb.jobit;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -24,7 +19,6 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -37,174 +31,172 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener, FirebaseAuth.AuthStateListener, GoogleApiClient.OnConnectionFailedListener {
-        private Button btnLogin, btnLoginGoogle, btnLoginFacebook, btnCreateAccount;
+    private Button btnLogin, btnLoginGoogle, btnLoginFacebook, btnCreateAccount;
 
-        private EditText edtEmail, edtPassword;
-        private GoogleApiClient apiClient;
-        private FirebaseAuth firebaseAuth;
-        CallbackManager callbackManager;
-        public static int REQUEST_CODE_LOGIN_GOOGLE = 3;
-        public static int REQUEST_CODE_LOGIN_FACEBOOK = 3;
+    private EditText edtEmail, edtPassword;
+    private GoogleApiClient apiClient;
+    private FirebaseAuth firebaseAuth;
+    CallbackManager callbackManager;
+    public static int REQUEST_CODE_LOGIN_GOOGLE = 3;
+    public static int REQUEST_CODE_LOGIN_FACEBOOK = 3;
 
-        public static int provider = -1;
-        public static int LOGIN_WITH_GMAIL = 0;
-        public static int LOGIN_WITH_GOOGLE = 1;
-        public static int LOGIN_WITH_FACEBOOK = 2;
+    public static int provider = -1;
+    public static int LOGIN_WITH_GMAIL = 0;
+    public static int LOGIN_WITH_GOOGLE = 1;
+    public static int LOGIN_WITH_FACEBOOK = 2;
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            FacebookSdk.sdkInitialize(getApplicationContext());
-            AppEventsLogger.activateApp(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
 
-            setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_in);
 
-            Window window = this.getWindow();
-            // clear FLAG_TRANSLUCENT_STATUS flag:
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            // finally change the color
-            window.setStatusBarColor(this.getResources().getColor(R.color.bgBlackTransparent40));
+        Window window = this.getWindow();
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        // finally change the color
+        window.setStatusBarColor(this.getResources().getColor(R.color.bgBlackTransparent40));
 
-            btnLogin = findViewById(R.id.btnLogin);
-            btnLoginGoogle = findViewById(R.id.btnLoginGoogle);
-            btnLoginFacebook = findViewById(R.id.btnLoginFacebook);
-            btnCreateAccount = findViewById(R.id.btnCreateAccount);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLoginGoogle = findViewById(R.id.btnLoginGoogle);
+        btnLoginFacebook = findViewById(R.id.btnLoginFacebook);
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
 
-            edtEmail = findViewById(R.id.edtEmailLogIn);
-            edtPassword = findViewById(R.id.edtpasswordLogin);
+        edtEmail = findViewById(R.id.edtEmailLogIn);
+        edtPassword = findViewById(R.id.edtpasswordLogin);
 
-            firebaseAuth = FirebaseAuth.getInstance();
-            firebaseAuth.signOut();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
 //            LoginManager.getInstance().logOut();
-            callbackManager = CallbackManager.Factory.create();
+        callbackManager = CallbackManager.Factory.create();
 
-            btnLoginGoogle.setOnClickListener(this);
-            btnLoginFacebook.setOnClickListener(this);
-            btnCreateAccount.setOnClickListener(this);
+        btnLoginGoogle.setOnClickListener(this);
+        btnLoginFacebook.setOnClickListener(this);
+        btnCreateAccount.setOnClickListener(this);
+        CreateClientGoogle();
+    }
 
-            } catch (NoSuchAlgorithmException e) {
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btnLogin:
+                break;
+            case R.id.btnLoginGoogle:
+                LoginWithGoogle();
+                break;
+            case R.id.btnLoginFacebook:
+                fbLogin();
+                break;
+            case R.id.btnCreateAccount:
+                Intent intent = new Intent(this, SignUpActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void fbLogin() {
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email", "public_profile"));
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        String tokenID = loginResult.getAccessToken().getToken();
+                        authenticateGoogle(tokenID);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+
+    private void CreateClientGoogle() {
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder()
+                .requestEmail()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .build();
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+    }
+
+    private void LoginWithGoogle() {
+        Intent iGoogle = Auth.GoogleSignInApi.getSignInIntent(apiClient);
+        provider = LOGIN_WITH_GOOGLE;
+        startActivityForResult(iGoogle, REQUEST_CODE_LOGIN_GOOGLE);
+    }
+
+    private void authenticateGoogle(String tokenID) {
+        if (provider == LOGIN_WITH_GOOGLE) {
+            AuthCredential authCredential = GoogleAuthProvider.getCredential(tokenID, null);
+            firebaseAuth.signInWithCredential(authCredential);
+        } else {
+            AuthCredential authCredential = FacebookAuthProvider.getCredential(tokenID);
+            firebaseAuth.signInWithCredential(authCredential);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE_LOGIN_GOOGLE) {
+            if (resultCode == RESULT_OK) {
+                GoogleSignInResult signInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                GoogleSignInAccount account = signInResult.getSignInAccount();
+                String tokenID = account.getIdToken();
+                authenticateGoogle(tokenID);
             }
-            CreateClientGoogle();
+        } else {
+            //callbackManager.onActivityResult(requestCode,resultCode,data);
         }
-
-        @Override
-        protected void onStart() {
-            super.onStart();
-            firebaseAuth.addAuthStateListener(this);
-        }
-
-        @Override
-        protected void onStop() {
-            super.onStop();
-            firebaseAuth.removeAuthStateListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-            int id = v.getId();
-            switch (id) {
-                case R.id.btnLogin:
-                    break;
-                case R.id.btnLoginGoogle:
-                    LoginWithGoogle();
-                    break;
-                case R.id.btnLoginFacebook:
-                    fbLogin();
-                    break;
-                case R.id.btnCreateAccount:
-                    Intent intent = new Intent(this,SignUpActivity.class);
-                    startActivity(intent);
-                    break;
-                default:
-                    break;
-            }
-        }
-        public void fbLogin() {
-            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email", "public_profile"));
-            LoginManager.getInstance().registerCallback(callbackManager,
-                    new FacebookCallback<LoginResult>() {
-                        @Override
-                        public void onSuccess(LoginResult loginResult) {
-                            // App code
-                            String tokenID = loginResult.getAccessToken().getToken();
-                            authenticateGoogle(tokenID);
-                        }
-
-                        @Override
-                        public void onCancel() {
-                            // App code
-                        }
-
-                        @Override
-                        public void onError(FacebookException exception) {
-                            // App code
-                        }
-                    });
-        }
-
-        private void CreateClientGoogle() {
-            GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder()
-                    .requestEmail()
-                    .requestIdToken(getString(R.string.default_web_client_id))
-                    .build();
-
-            apiClient = new GoogleApiClient.Builder(this)
-                    .enableAutoManage(this, this)
-                    .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
-                    .build();
-        }
-
-        private void LoginWithGoogle() {
-            Intent iGoogle = Auth.GoogleSignInApi.getSignInIntent(apiClient);
-            provider = LOGIN_WITH_GOOGLE;
-            startActivityForResult(iGoogle, REQUEST_CODE_LOGIN_GOOGLE);
-        }
-
-        private void authenticateGoogle(String tokenID) {
-            if (provider == LOGIN_WITH_GOOGLE) {
-                AuthCredential authCredential = GoogleAuthProvider.getCredential(tokenID, null);
-                firebaseAuth.signInWithCredential(authCredential);
-            } else {
-                AuthCredential authCredential = FacebookAuthProvider.getCredential(tokenID);
-                firebaseAuth.signInWithCredential(authCredential);
-            }
-        }
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-            if (requestCode == REQUEST_CODE_LOGIN_GOOGLE) {
-                if (resultCode == RESULT_OK) {
-                    GoogleSignInResult signInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-                    GoogleSignInAccount account = signInResult.getSignInAccount();
-                    String tokenID = account.getIdToken();
-                    authenticateGoogle(tokenID);
-                }
-            } else {
-                //callbackManager.onActivityResult(requestCode,resultCode,data);
-            }
-            callbackManager.onActivityResult(requestCode, resultCode, data);
-        }
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
 
-        @Override
-        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-            FirebaseUser user = firebaseAuth.getCurrentUser();
-            if (user != null) {
-                Toast.makeText(this, "Thanh Cong", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            Toast.makeText(this, "Thanh Cong", Toast.LENGTH_SHORT).show();
 //                Intent intent = new Intent(this, HomePageActivity.class);
 //                startActivity(intent);
-            } else {
+        } else {
 
-            }
         }
+    }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
