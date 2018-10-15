a646c711cd8d3fe400295ba2df695c1d53694395
MyLinhIT <1612334@student.hcmus.edu.vn>
2018-10-15 18:46:55 +0700

    Layout xml Home.
    Tạo giao diện home

---
+++ 04_Interface/I02_Source/JobIT/app/src/main/java/com/example/team32gb/jobit/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.team32gb.jobit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("loi","abc");
+        super.onCreate(savedInstanceState);
+        Log.i("loi","abc");
+        setContentView(R.layout.activity_home);
+        Log.i("loi","abc");
+    }
+}
